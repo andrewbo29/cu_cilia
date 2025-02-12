1dcb23a8966d95f1d85cecc7709e86513cc815bf
Kseniia Alekseitseva <kseniia.alekseitseva@constructor.tech>
2025-02-12 17:59:32 +0200

    prettified join main; wrote docstrings

--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
-from dotenv import load_dotenv
 import logging
 import os
-from pathlib import Path
 import warnings
+from pathlib import Path
+
+from dotenv import load_dotenv
+
 warnings.filterwarnings("ignore")
 
 from join_nuclei_and_cilia_results import main as join_nuclei_and_cilia_results
-from utils import compress_directory_to_zip, setup_logging
 from run_cilia_detection import main as run_cilia_detection
 from run_nuclei_segmentation import main as run_nuclei_segmentation
-
+from utils import compress_directory_to_zip, setup_logging
 
 load_dotenv("env.txt")
 setup_logging()
 
 logger = logging.getLogger("main")
 
+
 def _check_directory(path: Path):
+    """Checks if the given path exists and is a directory.
+
+    Args:
+        path: Path to check.
+
+    Raises:
+        RuntimeError: If the directory does not exist or the path is not a directory.
+    """
     if not path.exists():
         logger.error(f"Directory not found: '{path}'")
         raise RuntimeError(f"Directory not found: '{path}'")
@@ -26,11 +36,12 @@ def _check_directory(path: Path):
 
 
 def main():
+    """Main function to execute cilia detection, nuclei segmentation, merge results, and compress the output directory."""
     images_dir = Path(os.environ.get("IMAGES_DIR"))
     output_dir = Path(os.environ.get("OUTPUT_DIR"))
     logger.info(f"Processing data in {images_dir}")
     try:
-        n_files = _check_directory(images_dir)
+        _ = _check_directory(images_dir)
         logger.info(f"Saving results to {output_dir}")
         run_cilia_detection()
         run_nuclei_segmentation()
